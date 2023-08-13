2cdca61e1aef062c056fede6b7af7d33dfab3d87
svpudik <32640971+svpudik@users.noreply.github.com>
2023-08-13 19:39:26 +0200

    Update ESP8266_Control_Relay_with_sunrise_count.ino

--- ESP8266_Control_Relay_with_sunrise_count.ino
+++ ESP8266_Control_Relay_with_sunrise_count.ino
@@ -1,3 +1,4 @@
+ 
 #include <ESP8266WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
@@ -16,8 +17,8 @@ uint8_t R1Off[] = {0xA0, 0x03, 0x00, 0xA3};
 uint8_t R2On[] = {0xA0, 0x04, 0x01, 0xA5};
 uint8_t R2Off[] = {0xA0, 0x04, 0x00, 0xA4};
 
-const char* ssid = "OpenWrtextender"; // fill in here your router or wifi SSID
-const char* password = "77533791"; // fill in here your router or wifi password
+const char* ssid = "YourWifiSSID"; // fill in here your router or wifi SSID
+const char* password = "YourWifiPassword"; // fill in here your router or wifi password
 
 int Hours;
 int Minutes;
@@ -30,7 +31,7 @@ int sunrise;
 
 ESP8266WebServer server(80);
 
-const long utcOffsetInSeconds = 3600;
+const long utcOffsetInSeconds = 7200; // 3600 for winter 7200 summer
 unsigned char
 leap_days ;
 unsigned int
@@ -38,7 +39,14 @@ epoch, ntp_year, days_since_epoch, day_of_year;
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+NTPClient timeClient(ntpUDP, "195.113.144.238", utcOffsetInSeconds);
+IPAddress local_IP(192, 168, 3, 98);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 3, 254);
+
+IPAddress subnet(255, 255, 255, 0);
+//IPAddress primaryDNS(8, 8, 8, 8);   //optional
+//IPAddress secondaryDNS(8, 8, 4, 4); 
 
 void setup()
 {
@@ -52,7 +60,10 @@ void setup()
 
   Serial.println("initial timeout . . .");
   delay(1000 * 8);
-
+  // Configures static IP address
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+    Serial.println("STA Failed to configure");
+  }
   // Connect to WiFi network
   Serial.println();
   Serial.print("Connecting to ");
@@ -77,7 +88,6 @@ void setup()
 
   // Get time from web
   timeClient.begin();
-
   server.on("/", handle_OnConnect);
   server.onNotFound(handle_NotFound);
   server.begin();
@@ -101,6 +111,8 @@ void loop()
   day_of_year = ((days_since_epoch - leap_days) % 365) + 1;
   int sunset = (1144 - 144 * cos((day_of_year + 8) / 58.09))+30;
   int sunrise = (380 + 121 * cos((day_of_year + 8) / 58.09))-20;
+  Serial.println(timeClient.getFormattedTime());
+  Serial.println("day_of_year");
   Serial.println(day_of_year);
   sunriseHour = sunrise / 60;
   sunriseMin = sunrise % 60;
@@ -112,7 +124,7 @@ void loop()
     Serial.write(R1On, 4);
     delay(50);
     Serial.write(R2On, 4);
-    delay(1000 * 25);
+    delay(1000 * 60);
     Serial.write(R1Off, 4);
     delay(50);
     Serial.write(R2Off, 4);
@@ -142,13 +154,16 @@ void handle_OnConnect() {
 
   Minutes = timeClient.getMinutes();
   Hours = timeClient.getHours();
-
+  Serial.print(Hours);
+  Serial.print(":");
+  Serial.println(Minutes);
   server.send(200, "text/html", SendHTML(Hours, Minutes, sunriseHour, sunriseMin, sunsetHour, sunsetMin));
 }
 
 void handle_NotFound() {
   server.send(404, "text/plain", "Not found");
 }
+
 String SendHTML(float Hoursstat, float Minutesstat, float sunriseHourstat, float sunriseMinstat, float sunsetHourstat, float sunsetMinstat) {
   String ptr = "Current Time: ";
   ptr += (int)Hoursstat;
